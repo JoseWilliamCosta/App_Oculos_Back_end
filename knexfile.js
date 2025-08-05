6202ae9a7c56039c56db04d70b15e1973f901a04
davyslopes <davydloopes150@gmail.com>
2025-08-05 20:52:57 -0300

    ajustando erros do front

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
       port: '3306',
       database: 'sistestdb',
       user:     'root',
-      password: 'Liam2025!'
+      password: '123'
     },
     pool: {
       min: 2,
